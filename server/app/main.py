43761c32b73fcc00467e2403af35d87309509860
Evelyn Colon <evelyn.colon@ufl.edu>
2025-10-22 14:27:01 -0400

    feat: added CORS middleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import settings
+
+origins = [
+    settings.base_url,
+    settings.frontend_url
+]
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = origins,
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
 
 @app.get("/")
 async def read_root():
