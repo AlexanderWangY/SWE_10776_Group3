e87736a5716a2e6816a78e9340f351ecc7bf9dba
Evelyn Colon <evelyn.colon@ufl.edu>
2025-10-22 22:19:52 -0400

    feat: Added listings route to backend

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
+from app.api.listings import router as listings_router
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
@@ -11,6 +12,7 @@ origins = [
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(listings_router)
 app.add_middleware(
   CORSMiddleware,
   allow_origins = origins,
