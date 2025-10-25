830fb5e6f642ed300dba60325770b039b9506a8b
Evelyn Colon <evelyn.colon@ufl.edu>
2025-10-25 13:37:15 -0400

    fix: modified CORS middleware to allow frontend/backend connection

--- server/app/main.py
+++ server/app/main.py
@@ -16,6 +16,7 @@ app.include_router(listings_router)
 app.add_middleware(
   CORSMiddleware,
   allow_origins = origins,
+  allow_credentials = True,
   allow_methods = ["*"],
   allow_headers = ["*"]
 )
