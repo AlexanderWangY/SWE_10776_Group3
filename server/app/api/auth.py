8652c464713298d1678d7637d8b0c6fd6468241e
Evelyn Colon <evelyn.colon@ufl.edu>
2025-10-27 17:22:54 -0400

    added citation to pfp in auth

--- server/app/api/auth.py
+++ server/app/api/auth.py
@@ -47,7 +47,8 @@ async def verify_email(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="User is already verified.",
             )
-    
+
+# Source: https://github.com/fastapi/fastapi/discussions/9430   
 @router.get("/auth/me", tags=["auth"])
 async def get_me(
       user: User = Depends(fastapi_users.current_user())
