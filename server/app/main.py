b9003852858c5110aa1cc0b0df4ebe49ec13d653
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-11-30 15:14:41 -0600

    fixing stupid cors

--- server/app/main.py
+++ server/app/main.py
@@ -8,33 +8,35 @@ from app.core.config import settings
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, JSONResponse
 
-origins = [
-    settings.base_url,
-    settings.frontend_url
-]
+origins = [settings.base_url, settings.frontend_url]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins.extend(
+        ["https://www.gatormarket.com", "https://gatormarket.com"]
+    ),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(listings_router)
 app.include_router(profile_router)
 app.include_router(admin_router)
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins = origins,
-  allow_credentials = True,
-  allow_methods = ["*"],
-  allow_headers = ["*"]
-)
+
 
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the FastAPI application!"}
 
+
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 # @app.exception_handler(HTTPException)
 # async def auth_http_handler(request: Request, exc: HTTPException):
-#     if exc.status_code == 400: 
+#     if exc.status_code == 400:
 #         if exc.detail == "User is already verified.":
 #             return RedirectResponse(url=f"{settings.frontend_url}/login?verified=True")
 #         elif exc.detail == "Invalid token or user does not exist.":
